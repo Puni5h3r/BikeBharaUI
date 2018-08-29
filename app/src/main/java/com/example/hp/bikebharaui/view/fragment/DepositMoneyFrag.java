f20778a9eca8ad15c08a5bdd462ce9e18550f562
Puni5h3r <40525894+Puni5h3r@users.noreply.github.com>
2018-08-29 17:44:21 +0600

    All fragments done toolbar design left

--- app/src/main/java/com/example/hp/bikebharaui/view/fragment/DepositMoneyFrag.java
+++ app/src/main/java/com/example/hp/bikebharaui/view/fragment/DepositMoneyFrag.java
@@ -4,13 +4,18 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -20,9 +25,11 @@ public class DepositMoneyFrag extends BaseFragment implements AdapterView.OnItem
 
 
     private Button btnSave;
-    private Spinner spinner;
+    private Spinner spUserId;
     private Toolbar toolbar;
+    private EditText edtDepositAmount;
     Context mContext;
+    private TextInputLayout inputLayoutAmount;
 
     @Nullable
     @Override
@@ -32,16 +39,20 @@ public class DepositMoneyFrag extends BaseFragment implements AdapterView.OnItem
        mContext=getContext();
 
         btnSave = view.findViewById(R.id.button_deposit_save);
-        spinner = view.findViewById(R.id.spinner1);
+        spUserId = view.findViewById(R.id.spUserDeposity);
 
 
-        // setting the spinner
+        inputLayoutAmount = view.findViewById(R.id.input_layout_depositamount);
+        edtDepositAmount = view.findViewById(R.id.edtDepositAmount);
+
+
+        // setting the spUserId
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.dummyforspinner, android.R.layout.simple_spinner_item);
         //Specify the layout to use when the list choice appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        //Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
+        //Apply the adapter to the spUserId
+        spUserId.setAdapter(adapter);
+        spUserId.setOnItemSelectedListener(this);
 
 //        toolbar = findViewById(R.id.toolbar_deposite_money);
 //
@@ -54,10 +65,74 @@ public class DepositMoneyFrag extends BaseFragment implements AdapterView.OnItem
 //                startActivity(intent);
 //            }
 //        });
+
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                submitForm();
+            }
+        });
+
+
+        edtDepositAmount.addTextChangedListener(new MyTextWatcher(edtDepositAmount));
 //
 //
       return view;
     }
+
+    private void submitForm() {
+        if (!validateAmount()) {
+            return;
+        }
+
+        Toast.makeText(mContext, "Thank You!", Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean validateAmount() {
+        if (edtDepositAmount.getText().toString().trim().isEmpty()) {
+            inputLayoutAmount.setError(getString(R.string.err_msg_amount));
+            requestFocus(edtDepositAmount);
+            return false;
+        } else {
+            inputLayoutAmount.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+
+    private void requestFocus(View view) {
+        if (view.requestFocus()) {
+            getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        }
+    }
+
+    private class MyTextWatcher implements TextWatcher {
+
+        private View view;
+
+        private MyTextWatcher(View view) {
+            this.view = view;
+        }
+
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        public void afterTextChanged(Editable editable) {
+            switch (view.getId()) {
+                case R.id.edtDepositAmount:
+                    validateAmount();
+                    break;
+
+            }
+        }
+    }
+
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
--- app/src/main/java/com/example/hp/bikebharaui/view/fragment/LogRideMoneyFragment.java
+++ app/src/main/java/com/example/hp/bikebharaui/view/fragment/LogRideMoneyFragment.java
@@ -1,33 +1,36 @@
 package com.example.hp.bikebharaui.view.fragment;
 
 import android.content.Context;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.hp.bikebharaui.R;
 
-public class LogRideMoneyFragment extends BaseFragment implements AdapterView.OnItemSelectedListener{
+public class LogRideMoneyFragment extends BaseFragment implements AdapterView.OnItemSelectedListener {
 
     private Button btnSave;
-    private Spinner spinner;
+    private Spinner spUserId;
     private Toolbar toolbar;
     private Context mContext;
+    private EditText edtAmount;
+    private TextInputLayout inputLayoutAmount;
 
-    public LogRideMoneyFragment(){
-
+    public LogRideMoneyFragment() {
 
 
     }
@@ -36,12 +39,14 @@ public class LogRideMoneyFragment extends BaseFragment implements AdapterView.On
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.frag_log_ride_money,container,false);
+        View view = inflater.inflate(R.layout.frag_log_ride_money, container, false);
 
         btnSave = view.findViewById(R.id.button_log_ride_money_save);
-        spinner = view.findViewById(R.id.spinner2);
+        spUserId = view.findViewById(R.id.spUser);
+        edtAmount = view.findViewById(R.id.edtMoneyAmount);
+        inputLayoutAmount = view.findViewById(R.id.input_layout_moneyamount);
 
-        mContext=getContext();
+        mContext = getContext();
 
 //        toolbar=findViewById(R.id.toolbar_log_ride_money);
 //
@@ -55,19 +60,46 @@ public class LogRideMoneyFragment extends BaseFragment implements AdapterView.On
 //        });
 
 
-
-        // setting the spinner
+        // setting the spUserId
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.dummyforspinner, android.R.layout.simple_spinner_item);
         //Specify the layout to use when the list choice appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        //Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
+        //Apply the adapter to the spUserId
+        spUserId.setAdapter(adapter);
+        spUserId.setOnItemSelectedListener(this);
 
+        edtAmount.addTextChangedListener(new MyTextWatcher(edtAmount));
 
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                submitForm();
+            }
+        });
 
 
         return view;
+
+    }
+
+    private void submitForm() {
+        if (!validateAmount()) {
+            return;
+        }
+
+        Toast.makeText(mContext, "Thank You!", Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean validateAmount() {
+        if (edtAmount.getText().toString().trim().isEmpty()) {
+            inputLayoutAmount.setError(getString(R.string.err_msg_amount));
+            requestFocus(edtAmount);
+            return false;
+        } else {
+            inputLayoutAmount.setErrorEnabled(false);
+        }
+
+        return true;
     }
 
     @Override
@@ -77,13 +109,12 @@ public class LogRideMoneyFragment extends BaseFragment implements AdapterView.On
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        if (position==0){
-            Toast.makeText(mContext,"please select something from the list",Toast.LENGTH_LONG).show();
+        if (position == 0) {
+            Toast.makeText(mContext, "please select something from the list", Toast.LENGTH_LONG).show();
 
-        }
-        else {
+        } else {
             String j = parent.getItemAtPosition(position).toString();
-            Toast.makeText(mContext,j+" selected",Toast.LENGTH_LONG).show();
+            Toast.makeText(mContext, j + " selected", Toast.LENGTH_LONG).show();
 
         }
 
@@ -92,7 +123,39 @@ public class LogRideMoneyFragment extends BaseFragment implements AdapterView.On
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
-        Toast.makeText(mContext,"you haven't selected anything, please select",Toast.LENGTH_LONG).show();
+        Toast.makeText(mContext, "you haven't selected anything, please select", Toast.LENGTH_LONG).show();
 
+    }
+
+    private void requestFocus(View view) {
+        if (view.requestFocus()) {
+            getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        }
+    }
+
+
+    private class MyTextWatcher implements TextWatcher {
+
+        private View view;
+
+        private MyTextWatcher(View view) {
+            this.view = view;
+        }
+
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        public void afterTextChanged(Editable editable) {
+            switch (view.getId()) {
+                case R.id.edtMoneyAmount:
+                    validateAmount();
+                    break;
+
+            }
+        }
     }
 }
